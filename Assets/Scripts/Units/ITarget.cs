feb01c2ff3ad521afc63babf747093e7b9b24b9e
TaPet <134124527+TakenPt@users.noreply.github.com>
2026-03-15 16:10:04 +0900

    feat: 攻撃の対象となれるInterfaceを追加

---
+++ Assets/Scripts/Units/ITarget.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// 攻撃・防御側関係なく、攻撃の対象となるユニットのインターフェース
+/// </summary>
+public interface ITarget
+{
+    Vector3 GetTargetPosition();
+    Action<ITarget> OnDied { get; }
+}
