4d769a58abfae357c3954bb3f29e42c774f3e032
TaPet <134124527+TakenPt@users.noreply.github.com>
2026-03-15 16:12:13 +0900

    feat: ユニットの移動に関するinterfaceとそれを実装したBasicなクラスを追加

---
+++ Assets/Scripts/Units/MoveStrategy/IMoveStrategy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 盤面上で移動するユニットのインターフェース
+/// </summary>
+public interface IMoveStrategy
+{
+    void Move(Transform transform, float MoveSpeed);
+    void OnMapUpdated();
+}
+
