98f41fe7f428227acd1b82393abe2d390c3486be
ramtech629 <ran.ryu6629@gmail.com>
2026-03-25 20:25:22 +0900

    fix:GroundToCastleのGetBelowGridCellが動作するように、ユニット生成位置を少し浮かせる

--- Assets/Scripts/Input/PlayerCursorController.cs
+++ Assets/Scripts/Input/PlayerCursorController.cs
@@ -68,6 +68,7 @@ public class PlayerCursorController : MonoBehaviour
         {
             // アタッカーを生成
             _attackerSpawner.Spawn(transform.position, data); 
+            _attackerSpawner.Spawn(transform.position + new Vector3(0, 0.1f, 0), data);
         }
     }
 
