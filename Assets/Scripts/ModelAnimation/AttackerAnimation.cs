3e906be12cfd5ae0f8bbeb1a8913d3c7d6aa9b8d
TaPet <134124527+TakenPt@users.noreply.github.com>
2026-03-26 01:15:41 +0900

    feat: Attackerのアニメーションとテスト用シーンの追加 #38

---
+++ Assets/Scripts/ModelAnimation/AttackerAnimation.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AttackerAnimation : MonoBehaviour, IAttackableAnimation
+{
+    [SerializeField]
+    private GameObject _normal;
+
+    [SerializeField]
+    private GameObject _attacking;
+
+    [field: SerializeField]
+    public Image UnitImage { get; }
+
+    private int _attackingLeftFlames = 0;
+    private bool _isSubscribed = false;
+
+    void Awake()
+    {
+        _attacking.SetActive(false);
+        _normal.SetActive(true);
+    }
+
+    public void AttackAnimation(int flames)
+    {
+        if (flames <= 0) return;
+
+        _attackingLeftFlames = flames;
+
+        if (!_isSubscribed)
+        {
+            if (GameManager.Instance != null)
+            {
+                GameManager.Instance.OnTickEvent += CountFlame;
+                _isSubscribed = true;
+            }
+        }
+
+        if (_normal.activeSelf)
+            _normal.SetActive(false);
+
+        if (!_attacking.activeSelf)
+            _attacking.SetActive(true);
+    }
+
+    public void CountFlame()
+    {
+        _attackingLeftFlames--;
+
+        if (_attackingLeftFlames <= 0)
+        {
+            _attackingLeftFlames = 0;
+
+            if (!_normal.activeSelf)
+                _normal.SetActive(true);
+
+            if (_attacking.activeSelf)
+                _attacking.SetActive(false);
+
+            if (_isSubscribed && GameManager.Instance != null)
+            {
+                GameManager.Instance.OnTickEvent -= CountFlame;
+                _isSubscribed = false;
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        // 念のため解除（破棄・非アクティブ時の事故防止）
+        if (_isSubscribed && GameManager.Instance != null)
+        {
+            GameManager.Instance.OnTickEvent -= CountFlame;
+            _isSubscribed = false;
+        }
+    }
+}
