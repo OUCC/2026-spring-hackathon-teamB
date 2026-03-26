79a6f79318cae85670597828aa28510e288ed3a2
TaPet <134124527+TakenPt@users.noreply.github.com>
2026-03-26 22:46:35 +0900

    feat: 城の位置の変更と城の3dデータの使用 #42

--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -17,6 +17,9 @@ public class GridManager : MonoBehaviour
     [SerializeField] private Color _outlineColor = new Color(0f, 0f, 0f, 0.5f);
     [SerializeField] private float _voxelResolution = 16f;
 
+    [Header("Caslte Settings")]
+    [SerializeField] private GameObject _castlePrefab;
+
     public int Width => _width;
     public int Height => _height;
     public float CellSize => _cellSize;
@@ -93,6 +96,20 @@ public class GridManager : MonoBehaviour
                 _logicalGrid[x, z] = new CellData(x, z, cell);
                 cell.CellData = _logicalGrid[x, z];
             }
+        }
+
+        if (_castlePrefab != null)
+        {
+            Vector3 castlePosition = new Vector3((_width / 2) * _cellSize, 1, 0);
+            GameObject castle = Instantiate(_castlePrefab, castlePosition, Quaternion.identity, transform);
+
+            var cellBound = Castle.GridCell.GetComponent<Renderer>().bounds;
+            var castleBoound = castle.GetComponentInChildren<Renderer>().bounds;
+
+            float offset = cellBound.max.z - castleBoound.min.z;
+            castle.transform.position += new Vector3(0, 0, offset);
+
+            castle.name = "Castle";
         }
 
         CalcDirectionToCell(-1, -1);
@@ -319,7 +336,7 @@ public class GridManager : MonoBehaviour
     {
         get
         {
-            return GetCellData(0, _height / 2);
+            return GetCellData(_width / 2, _height - 1);
         }
     }
     public bool CanEnter(int x, int z)
