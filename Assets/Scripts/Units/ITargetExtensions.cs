b0aee88179b6f0f92bf3daa6d9d97f4976eb35b5
TaPet <134124527+TakenPt@users.noreply.github.com>
2026-03-26 20:46:20 +0900

    feat: IEnumerable<ITarget>をある地点との距離でソートする拡張メソッドを追加 #49

---
+++ Assets/Scripts/Units/ITargetExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+public static class ITargetExtensions
+{
+    public static IOrderedEnumerable<ITarget> OrderByDistance(this IEnumerable<ITarget> source, Vector3 sourcePositon)
+    {
+        return source.OrderBy(t => (t.gameObject.transform.position - sourcePositon).sqrMagnitude);
+    }
+}
