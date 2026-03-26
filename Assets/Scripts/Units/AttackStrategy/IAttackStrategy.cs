464dc12a084c142f3599719605db990df175b35a
TaPet <134124527+TakenPt@users.noreply.github.com>
2026-03-26 23:34:56 +0900

    feat: add `using ZLinq;` #56

--- Assets/Scripts/Units/AttackStrategy/IAttackStrategy.cs
+++ Assets/Scripts/Units/AttackStrategy/IAttackStrategy.cs
@@ -4,6 +4,8 @@ using System.Linq;
 
 using UnityEngine;
 
+using ZLinq;
+
 public interface IAttackStrategy
 {
     bool IsCooling { get { return RemainingCooldown > 0; } }
--- Assets/Scripts/Units/ITargetExtensions.cs
+++ Assets/Scripts/Units/ITargetExtensions.cs
@@ -3,6 +3,8 @@ using System.Linq;
 
 using UnityEngine;
 
+using ZLinq;
+
 public static class ITargetExtensions
 {
     public static IOrderedEnumerable<ITarget> OrderByDistance(this IEnumerable<ITarget> source, Vector3 sourcePositon)
