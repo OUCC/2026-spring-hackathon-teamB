4f13f8af6f96019e677dbced158dc91f51e32af1
TaPet <134124527+TakenPt@users.noreply.github.com>
2026-03-26 16:58:42 +0900

    feat: 守り側弓兵の3dデータ適用済みプレハブの作成 #47

--- Assets/Scripts/ModelAnimation/DefenderArcherAnimation.cs
+++ Assets/Scripts/ModelAnimation/DefenderArcherAnimation.cs
@@ -1,5 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UIElements;
 
-public class DefenderArcherAnimation : MonoBehaviour
+public class DefenderArcherAnimation : MonoBehaviour, IAttackAnimation
 {
+    [field: SerializeField]
+    public Image UnitImage { get; }
+
+    public void AttackAnimation(int flames)
+    {
+        return;
+    }
 }
